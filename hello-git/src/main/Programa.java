54c3047bd3d434f4264d7cd4c7c1e1466912403e
Aleph Costa Melo <aleph.melo@sgsistemas.com.br>
2022-09-02 10:47:07 -0300

    Novo Print

--- hello-git/src/main/Programa.java
+++ hello-git/src/main/Programa.java
@@ -4,6 +4,7 @@ public class Programa {
     public static void main(String[] args) {
         String texto = "hello git";
         System.out.println(texto);
+        System.out.println("Ola git.");
         imprimir();
     }
     public  static void imprimir(){
