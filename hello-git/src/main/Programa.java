dd668add41fd7c4148fb01dc8e9bc2db2bdead47
Aleph Costa Melo <aleph.melo@sgsistemas.com.br>
2022-09-02 10:36:27 -0300

    metado imprimir

--- hello-git/src/main/Programa.java
+++ hello-git/src/main/Programa.java
@@ -4,8 +4,11 @@ public class Programa {
     public static void main(String[] args) {
         String texto = "hello git";
         System.out.println(texto);
+        imprimir();
     }
-
+    public  static void imprimir(){
+        System.out.println("teste do metado imprimir");
+    }
 
 
 }
